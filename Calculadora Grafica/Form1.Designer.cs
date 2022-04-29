12f65f3a3fb3e0f53398cbbb0aa7746d89002d00
AugustoOL <auoliveira43@gmail.com>
2022-04-29 17:30:49 -0300

    first comit

---
+++ Calculadora Grafica/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Calculadora_Grafica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">verdade se for necessário descartar os recursos gerenciados; caso contrário, falso.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte do Designer - não modifique
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbCalculos = new System.Windows.Forms.GroupBox();
+            this.rbArea = new System.Windows.Forms.RadioButton();
+            this.rbPerimetro = new System.Windows.Forms.RadioButton();
+            this.rbVolume = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lbResultado = new System.Windows.Forms.Label();
+            this.gbCalculos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbCalculos
+            // 
+            this.gbCalculos.Controls.Add(this.rbVolume);
+            this.gbCalculos.Controls.Add(this.rbPerimetro);
+            this.gbCalculos.Controls.Add(this.rbArea);
+            this.gbCalculos.Location = new System.Drawing.Point(145, 23);
+            this.gbCalculos.Name = "gbCalculos";
+            this.gbCalculos.Size = new System.Drawing.Size(146, 86);
+            this.gbCalculos.TabIndex = 0;
+            this.gbCalculos.TabStop = false;
+            this.gbCalculos.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rbArea
+            // 
+            this.rbArea.AutoSize = true;
+            this.rbArea.Checked = true;
+            this.rbArea.ForeColor = System.Drawing.Color.DarkOrange;
+            this.rbArea.Location = new System.Drawing.Point(7, 11);
+            this.rbArea.Name = "rbArea";
+            this.rbArea.Size = new System.Drawing.Size(106, 17);
+            this.rbArea.TabIndex = 1;
+            this.rbArea.TabStop = true;
+            this.rbArea.Text = "AREA CIRCULO";
+            this.rbArea.UseVisualStyleBackColor = true;
+            this.rbArea.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbPerimetro
+            // 
+            this.rbPerimetro.AutoSize = true;
+            this.rbPerimetro.Location = new System.Drawing.Point(7, 34);
+            this.rbPerimetro.Name = "rbPerimetro";
+            this.rbPerimetro.Size = new System.Drawing.Size(138, 17);
+            this.rbPerimetro.TabIndex = 2;
+            this.rbPerimetro.Text = "PERIMETRO CIRCULO";
+            this.rbPerimetro.UseVisualStyleBackColor = true;
+            // 
+            // rbVolume
+            // 
+            this.rbVolume.AutoSize = true;
+            this.rbVolume.Location = new System.Drawing.Point(7, 58);
+            this.rbVolume.Name = "rbVolume";
+            this.rbVolume.Size = new System.Drawing.Size(123, 17);
+            this.rbVolume.TabIndex = 3;
+            this.rbVolume.Text = "VOLUME CIRCULO";
+            this.rbVolume.UseVisualStyleBackColor = true;
+            this.rbVolume.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DIGITE O RAIO:";
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(15, 49);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(87, 20);
+            this.txtRaio.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnCalcular.ForeColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(15, 75);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(87, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.Location = new System.Drawing.Point(12, 112);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(128, 20);
+            this.lbResultado.TabIndex = 4;
+            this.lbResultado.Text = "RESPOSTA = 0";
+            this.lbResultado.Click += new System.EventHandler(this.lbResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(304, 142);
+            this.Controls.Add(this.lbResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtRaio);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbCalculos);
+            this.Font = new System.Drawing.Font("Rockwell", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DarkOrange;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULADORA GRÁFICA";
+            this.gbCalculos.ResumeLayout(false);
+            this.gbCalculos.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbCalculos;
+        private System.Windows.Forms.RadioButton rbArea;
+        private System.Windows.Forms.RadioButton rbVolume;
+        private System.Windows.Forms.RadioButton rbPerimetro;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lbResultado;
+    }
+}
+
---
+++ Calculadora Grafica/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Grafica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (rbArea.Checked == true)
+            {
+                double raio = Convert.ToDouble(txtRaio.Text);
+                double area = Math.PI * Math.Pow(raio, 2);
+                lbResultado.Text = "AREA =" + area.ToString("N3");
+            }
+            if (rbPerimetro.Checked == true)
+            {
+                double raio = Convert.ToDouble(txtRaio.Text);
+                double perimetro = 2 * Math.PI * raio;
+                lbResultado.Text = "PERIMETRO = "
+                    + perimetro.ToString("N3");
+            }
+            if (rbVolume.Checked == true)
+            {
+                double raio = Convert.ToDouble(txtRaio.Text);
+                double volume = 3f / 4f * Math.PI * Math.Pow(raio, 3);
+                lbResultado.Text = "VOLUME =" + volume.ToString("N3");
+            }
+        }
+
+        private void lbResultado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
